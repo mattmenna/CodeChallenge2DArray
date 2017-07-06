21af04a1837dabcd20792508a2e4a0225781195f
Matthew Menna <mattmenna@gmail.com>
2017-07-06 10:38:05 -0400

    Added Swithc and new method

--- src/Array2DChallenge.java
+++ src/Array2DChallenge.java
@@ -4,20 +4,49 @@ import java.util.Scanner;
  * Created by Steve on 7/6/2017.
  */
 public class Array2DChallenge {
-    public static void main(String[] args) {
 
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int[][] arr = new int[3][3];
 
         System.out.println("We are going to create a 3 x 3 array.");
 
-        userFilledArray(arr);
+        System.out.println("Choose the method to fill\n Element by element : 1\nRow : 2");
+        int caseNumber = scan.nextInt();
+        switch (caseNumber){
+            case 1:
+                userFilledArray(arr, scan);
+                break;
+            case 2:
+                fillByRow(arr, scan);
+                break;
+            default:
+                break;
+        }
+
 
         System.out.println("The sum of the hourglass is: " + getSum(arr));
 
     }
 
-    private static int[][] userFilledArray(int arr[][]){
-        Scanner scan = new Scanner(System.in);
+    private static int[][] userFilledArray(int arr[][], Scanner scan){
+
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                System.out.print("Enter integers for row " + (i+1) + " column " + (j+1) + ": ");
+                int userNum = scan.nextInt();
+
+                arr[i][j] = userNum;
+            }
+        }
+        return arr;
+    }
+
+    private static int[][] fillByRow(int arr[][], Scanner scan){
+
         for (int i = 0; i < arr.length ; i++) {
             for (int j = 0; j < arr[i].length; j++) {
 
